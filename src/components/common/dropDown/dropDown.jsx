981057ff432c4ab1f6ac717810164d925d2cc3af
Miaad Estakhri <miaad8104@gmail.com>
2023-05-26 21:51:01 +0330

    create handleClick for open and close dropDown

--- src/components/common/dropDown/dropDown.jsx
+++ src/components/common/dropDown/dropDown.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 const DropDown = () => {
   const [isShow, SetIsShow] = useState(false);
+  const handleClick = (e) => {
+    SetIsShow(isShow === true ? false : true);
+  };
 
   return (
-    <section>
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
-      <div>D</div>
-      <div>D</div>
+    <section className="" onClick={handleClick}>
+      {isShow === true ? "<" : ">"}
+      <div className={`${isShow ? "d-flex flex-column" : "d-none"}`}>
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+        <div>D</div>
+      </div>
     </section>
   );
 };
