e962c5e487156dfd783d284d9a596d7d4d924b22
Miaad Estakhri <miaad8104@gmail.com>
2023-05-26 19:15:36 +0330

    Create DropDown component

---
+++ src/components/common/dropDown/dropDown.jsx
@@ -0,0 +1,13 @@
+const DropDown = () => {
+  return (
+    <section>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default DropDown;
