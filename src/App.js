3933c5722e62908781badd31fe6862d4ce981bf9
Miaad Estakhri <miaad8104@gmail.com>
2023-05-26 22:06:16 +0330

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Test from "./components/test";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <Test />
     </div>
   );
