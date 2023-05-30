d706dd1b4f0382bf3dda5d330b154b31d51c2c29
Miaad Estakhri <miaad8104@gmail.com>
2023-05-30 19:42:37 +0330

    install bootstrap and import on app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Test from "./components/test";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
