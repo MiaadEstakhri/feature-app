dabee30dc56abfb766c926b039531300315f59db
Miaad Estakhri <miaad8104@gmail.com>
2023-05-30 20:30:18 +0330

    use Method map( ) and use useState and create isOpen

--- src/components/common/dropDown/dropDown.jsx
+++ src/components/common/dropDown/dropDown.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
+const data = [
+  { name: "ali" },
+  { name: "miaad" },
+  { name: "fozhan" },
+  { name: "milad" },
+];
+
 const DropDown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <section>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </section>
+    <div className="d-flex flex-column justify-content-center align-items-center border-1 border-black">
+      {data.map((item, key) => {
+        return (
+          <div key={key}>
+            <div>{item.name}</div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
